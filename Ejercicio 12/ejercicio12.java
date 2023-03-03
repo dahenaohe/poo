4f2e79385f3004d58779daff7b9a8c86d7f8dfde
David Henao <125745988+dahenaohe@users.noreply.github.com>
2023-03-03 13:35:47 -0500

    Delete Ejercicio 12 directory

--- Ejercicio 12/ejercicio12.java
+++
@@ -1,9 +0,0 @@
-public class ejercicio12 {
-    public static void main(String[] args){
-        double HT = 48; //Horas trabajadas
-        double PH = 5000; //Pago por hora
-        double SB = HT * PH; //Salario bruto
-        double SN = SB - (SB * 0.125); //Salario neto
-        System.out.println("El salario bruto es: " + SB + "semanal" + "\nEl salario neto es: " + SN + "semanal");
-    }
-}
