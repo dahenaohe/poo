f5ad5e87db630a35c9160a1cf3c7d1ecfec61c83
David Henao <125745988+dahenaohe@users.noreply.github.com>
2023-05-22 17:28:18 -0500

    Update VentanaCalculadora.java

--- Actividad 3/Ejercicio 7/VentanaCalculadora.java
+++ Actividad 3/Ejercicio 7/VentanaCalculadora.java
@@ -4,51 +4,67 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 public class VentanaCalculadora extends JFrame {
-    private JTextField txtNumero;
+    private JTextArea txtNumeros;
     private JButton btnCalcular;
 
     public VentanaCalculadora() {
-        setTitle("Calculadora de raíz cuadrada, cuadrado y cubo");
-        setSize(400, 150);
+        setTitle("Calculadora de raíz cuadrada, cuadrado y cubo para un grupo de números");
+        setSize(400, 200);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        setLayout(new GridLayout(2, 3));
-
-        add(new JLabel("Número:"));
-        txtNumero = new JTextField();
-        add(txtNumero);
-
+        setLayout(new BorderLayout());
+    
+        JPanel instruccionesPanel = new JPanel(new BorderLayout());
+        JLabel lblInstrucciones = new JLabel("Ingrese números enteros separados por comas o espacios:");
+        instruccionesPanel.add(lblInstrucciones, BorderLayout.NORTH);
+    
+        txtNumeros = new JTextArea();
+        instruccionesPanel.add(new JScrollPane(txtNumeros), BorderLayout.CENTER);
+        add(instruccionesPanel, BorderLayout.CENTER);
+    
         btnCalcular = new JButton("Calcular");
-        add(btnCalcular);
+        add(btnCalcular, BorderLayout.SOUTH);
         btnCalcular.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
                 calcularOperaciones();
             }
         });
-        add(new JLabel());
     }
-
     private void calcularOperaciones() {
-        int numero = Integer.parseInt(txtNumero.getText());
+        String[] numerosStr = txtNumeros.getText().split("\\s*,?\\s*");
+        StringBuilder resultados = new StringBuilder();
 
-        double raizCuadrada = Calculadora.raizCuadrada(numero);
-        int cuadrado = Calculadora.cuadrado(numero);
-        int cubo = Calculadora.cubo(numero);
+        for (String numeroStr : numerosStr) {
+            if (numeroStr.isEmpty()) {
+                continue;
+            }
+            try {
+                int numero = Integer.parseInt(numeroStr);
+
+                double raizCuadrada = Calculadora.raizCuadrada(numero);
+                int cuadrado = Calculadora.cuadrado(numero);
+                int cubo = Calculadora.cubo(numero);
+
+                resultados.append("Número: ").append(numero).append("\n");
+                resultados.append("Raíz cuadrada: ").append(String.format("%.2f", raizCuadrada)).append("\n");
+                resultados.append("Cuadrado: ").append(cuadrado).append("\n");
+                resultados.append("Cubo: ").append(cubo).append("\n\n");
+            } catch (NumberFormatException e) {
+                JOptionPane.showMessageDialog(this, "Por favor ingrese números enteros separados por comas o espacios", "Error", JOptionPane.ERROR_MESSAGE);
+                return;
+            }
+        }
 
         JDialog resultadosDialog = new JDialog(this, "Resultado", true);
-        resultadosDialog.setSize(400, 150);
-        resultadosDialog.setLayout(new GridLayout(3, 2));
+        resultadosDialog.setSize(400, 300);
 
-        resultadosDialog.add(new JLabel("Raíz cuadrada:"));
-        resultadosDialog.add(new JLabel(String.format("%.2f", raizCuadrada)));
-
-        resultadosDialog.add(new JLabel("Cuadrado:"));
-        resultadosDialog.add(new JLabel(String.valueOf(cuadrado)));
-
-        resultadosDialog.add(new JLabel("Cubo:"));
-        resultadosDialog.add(new JLabel(String.valueOf(cubo)));
+        JTextArea resultadosArea = new JTextArea(resultados.toString());
+        resultadosArea.setEditable(false);
+        JScrollPane scrollPane = new JScrollPane(resultadosArea);
+        resultadosDialog.add(scrollPane);
 
         resultadosDialog.setLocationRelativeTo(this);
         resultadosDialog.setVisible(true);
     }
 }
+    
